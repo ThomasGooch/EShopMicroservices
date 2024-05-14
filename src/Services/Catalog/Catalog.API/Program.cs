16c4e1fd230b61e80646e8d50757e1b3acdc0240
Thomas Gooch <Thomas.Gooch@McKesson.com>
2024-05-14 16:04:26 -0400

    setup postgresql docker compose

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// add services
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// configure pipeline
+
+app.MapCarter();
 
 app.Run();
