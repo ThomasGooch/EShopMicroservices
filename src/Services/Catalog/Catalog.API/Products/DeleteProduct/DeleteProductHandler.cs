b872ba3743a0d61865d3179e45b93fb9c76d3305
Thomas Gooch <Thomas.Gooch@McKesson.com>
2024-05-21 11:51:12 -0400

    added delete and update usecases

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct
+{
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+    public class DeleteProductCommandHandler(IDocumentSession session, ILogger<DeleteProductCommandHandler> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", command);
+
+            var productToDelete = await session.LoadAsync<Product>(command.Id, cancellationToken) ?? throw new ProductNotFoundException();
+
+            session.Delete(productToDelete);
+
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool IsSuccess);
+    public class UpdateProductCommandHandler(IDocumentSession session, ILogger<UpdateProductCommandHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            // business logic to update an existing product
+            logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Command}", command);
+
+            // find a Product entity from command
+            var productForUpdate = await session.LoadAsync<Product>(command.Id, cancellationToken) ?? throw new ProductNotFoundException();
+
+            productForUpdate.Name = command.Name;
+            productForUpdate.Categories = command.Categories;
+            productForUpdate.Description = command.Description;
+            productForUpdate.ImageFile = command.ImageFile;
+            productForUpdate.Price = command.Price;
+
+
+            // save changes to the database
+            session.Update(productForUpdate);
+            await session.SaveChangesAsync(cancellationToken);
+            // return CreateProductResult result
+            return new UpdateProductResult(true);
+        }
+    }
+
+}
