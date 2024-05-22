909708646b11bed5fce293c93254afb99ed2c3c5
Thomas Gooch <Thomas.Gooch@McKesson.com>
2024-05-22 11:06:04 -0400

    implemented validations for all commands

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandValidator.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandValidator.cs
@@ -4,9 +4,9 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
 {
     public CreateProductCommandValidator()
     {
-        RuleFor(x => x.Name).NotEmpty();
-        RuleFor(x => x.Categories).NotEmpty();
-        RuleFor(x => x.ImageFile).NotEmpty();
-        RuleFor(x => x.Price).GreaterThan(0);
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is REQUIRED.");
+        RuleFor(x => x.Categories).NotEmpty().WithMessage("Categories is REQUIRED.");
+        RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is REQUIRED.");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
     }
 }
